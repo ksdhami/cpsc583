5b1e8540b58bf5c1907de92c84194f8ef199a240
ksdhami <32716134+ksdhami@users.noreply.github.com>
2019-03-19 17:38:29 -0600

    Update parallel.js

--- parallel.js
+++ parallel.js
@@ -372,13 +372,13 @@ function setupParallel3() {
     })
 }
 
-var renderQueue = (function(func) {
-    var _queue = [],                  // data to be rendered
-        _rate = 1000,                 // number of calls per frame
+let renderQueue = (function(func) {
+    let _queue = [],                  // data to be rendered
+        _rate = 30,                 // number of calls per frame
         _invalidate = function() {},  // invalidate last render queue
         _clear = function() {};       // clearing function
 
-    var rq = function(data) {
+    let rq = function(data) {
         if (data) rq.data(data);
         _invalidate();
         _clear();
@@ -401,43 +401,16 @@ var renderQueue = (function(func) {
         doFrame();
     };
 
+    rq.invalidate = _invalidate;
+
     rq.data = function(data) {
         _invalidate();
         _queue = data.slice(0);   // creates a copy of the data
         return rq;
     };
 
-    rq.add = function(data) {
-        _queue = _queue.concat(data);
-    };
-
-    rq.rate = function(value) {
-        if (!arguments.length) return _rate;
-        _rate = value;
-        return rq;
-    };
-
-    rq.remaining = function() {
-        return _queue.length;
-    };
-
-    // clear the canvas
-    rq.clear = function(func) {
-        if (!arguments.length) {
-            _clear();
-            return rq;
-        }
-        _clear = func;
-        return rq;
-    };
-
-    rq.invalidate = _invalidate;
-
-    var timer_frame = window.requestAnimationFrame
+    let timer_frame = window.requestAnimationFrame
         || window.webkitRequestAnimationFrame
-        || window.mozRequestAnimationFrame
-        || window.oRequestAnimationFrame
-        || window.msRequestAnimationFrame
         || function(callback) { setTimeout(callback, 17); };
 
     return rq;
