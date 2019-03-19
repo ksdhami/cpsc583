dd1ab80d0fb537f06516dadb3c5cf5d42d5a6782
ksdhami <32716134+ksdhami@users.noreply.github.com>
2019-03-19 17:34:34 -0600

    Update parallel.js

--- parallel.js
+++ parallel.js
@@ -1,8 +1,12 @@
 // http://bl.ocks.org/syntagmatic/raw/3341641/
+// https://gist.github.com/syntagmatic/3341641
 // https://github.com/d3/d3-brush
+// http://bl.ocks.org/syntagmatic/0d1635533f6fb5ac4da3
 // https://stackoverflow.com/questions/4394309/how-do-i-fit-an-image-img-inside-a-div-and-keep-the-aspect-ratio
 // https://stackoverflow.com/questions/4476526/do-i-use-img-object-or-embed-for-svg-files
 // https://d3js.live/#/Parallel_Coordinates
+// https://bocoup.com/blog/d3js-and-canvas
+// https://www.pluralsight.com/blog/software-development/d3-tutorial-canvas-d3-data-visualization
 
 window.onload = function(){
     setupParallel1();
@@ -35,26 +39,15 @@ let dateType = {
     yScale: d3.scaleTime().range([height-2, 0])
 };
 
-var column = [
-    {key: "Manufacturer", description: "Winning Manufacturer", type: strType,},
-    {key: "Year", description: "Year of Race", type: dateType,},
-    {key: "EngineSizeLitre", description: "Engine Size (l)", type: numType,},
-    {key: "NoCylinders", description: "Number of Engine Cylinders", type: numType,},
-    {key: "Distance", description: "Distance Traveled (km)", type: numType,},
-    {key: "AveSpeed", description: "Average Lap Speed (km/h)", type: numType,},
-    {key: "Laps", description: "Number of Laps Completed", type: numType,},
-    {key: "FastestSpeed", description: "Fastest Recorded Speed (kph)", type: numType,},
-    {key: "CircuitDesign", description: "Circuit Design", type: numType,},
-    {key: "Deaths", description: "Number of Fatalities", type: numType,},
-    {key: "Qualified", description: "Qualified Position", type: numType,}
-];
+let color = d3.scaleOrdinal()
+    .domain(["CHENARD & WALCKER", "BENTLEY", "LORRAINE DIETRICH", "ALFA-ROMEO", "LAGONDA", "BUGATTI", "DELAHAYE", "FERRARI", "TALBOT LAGO", "JAGUAR", "MERCEDES- BENZ", "ASTON MARTIN", "FORD", "PORSCHE", "MATRA- SIMCA", "GULF", "RENAULT ALPINE", "SAUBER", "MAZDA", "PEUGEOT", "McLAREN", "BMW", "AUDI" ])
+    .range(d3["schemeDark2"]);
 
 function setupParallel1() {
     let xScale1, yAxis1, svg1, canvas1, context1, axes1;
     let pixelRatio1 = window.devicePixelRatio;
 
     var column1 = [
-        //{key: "Manufacturer", description: "Winning Manufacturer", type: strType,},
         {key: "CircuitDesign", description: "Circuit Design", type: numType,},
         {key: "Year", description: "Year of Race", type: dateType,},
         {key: "EngineSizeLitre", description: "Engine Size (l)", type: numType,},
@@ -73,30 +66,26 @@ function setupParallel1() {
 
     yAxis1 = d3.axisLeft();
 
+    // For Axis
     svg1 = d3.select("#svg_vis1")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // For Lines
     canvas1 = d3.select("#canvas_vis1")
-        .attr("width", width * pixelRatio1)
-        .attr("height", height * pixelRatio1)
-        .style("width", width + "px")
-        .style("height", height + "px")
+        .attr("width", width * pixelRatio1 + "px")
+        .attr("height", height * pixelRatio1 + "px")
         .style("margin-top", margin.top + "px")
         .style("margin-left", margin.left + "px");
 
     context1 = canvas1.node().getContext("2d");
-    context1.globalCompositeOperation = 'darken';
-    context1.globalAlpha = 0.5;
-    context1.lineWidth = 2;
-    context1.scale(pixelRatio1, pixelRatio1);
 
     axes1 = svg1.selectAll(".axis")
         .data(column1)
         .enter().append("g")
-        .attr("class", function(d) { return "axis " + d.key.replace(/ /g, "_"); })
+        .attr("class", function(d) { return "axis " + d.key; })
         .attr("transform", function(d,i) { return "translate(" + xScale1(i) + ")"; });
 
     d3.csv("lemans4.csv").then(function(data) {
@@ -108,15 +97,25 @@ function setupParallel1() {
         data.forEach(function(d) {
             column1.forEach(function(e) {
                 d[e.key] = !d[e.key] ? null : e.type.coerce(d[e.key]);
+                e.domain = e.type.extent(data.map(function(d) {
+                    return d[e.key];
+                }));
+                e.scale = e.type.yScale.copy().domain(e.domain);
             });
         });
 
-        column1.forEach(function(dim) {
-            dim.domain = dim.type.extent(data.map(function(d) { return d[dim.key]; }));
-            dim.scale = dim.type.yScale.copy().domain(dim.domain);
+        let render1 = renderQueue(function (d) {
+            context1.strokeStyle = color1(d.CircuitDesign);
+            context1.beginPath();
+            let coords = project(d);
+            coords.forEach(function(d) {
+                if (d === null) {
+                    return;
+                }
+                context1.lineTo(d[0],d[1]);
+            });
+            context1.stroke();
         });
-
-        let render1 = renderQueue(draw).rate(1000);
 
         render1(data);
 
@@ -131,22 +130,6 @@ function setupParallel1() {
                 return d.description;
             });
 
-        d3.selectAll(".axis.Manufacturer .tick text")
-            .style("fill", "black");
-
-        function draw(d) {
-            context1.strokeStyle = color1(d.Manufacturer);
-            context1.beginPath();
-            let coords = project(d);
-            coords.forEach(function(d) {
-                if (d === null) {
-                    return;
-                }
-                context1.lineTo(d[0],d[1]);
-            });
-            context1.stroke();
-        }
-
         function project(d) {
             return column1.map(function(p, i) {
                 return d[p.key] === null ? null : [xScale1(i),p.scale(d[p.key])];
@@ -159,8 +142,22 @@ function setupParallel2() {
     let xScale2, yAxis2, svg2, canvas2, context2, axes2;
     let pixelRatio2 = window.devicePixelRatio;
 
+    var column2 = [
+        {key: "Manufacturer", description: "Winning Manufacturer", type: strType,},
+        {key: "Year", description: "Year of Race", type: numType,},
+        {key: "EngineSizeLitre", description: "Engine Size (l)", type: numType,},
+        {key: "NoCylinders", description: "Number of Engine Cylinders", type: numType,},
+        {key: "Distance", description: "Distance Traveled (km)", type: numType,},
+        {key: "AveSpeed", description: "Average Lap Speed (km/h)", type: numType,},
+        {key: "Laps", description: "Number of Laps Completed", type: numType,},
+        {key: "FastestSpeed", description: "Fastest Recorded Speed (kph)", type: numType,},
+        {key: "CircuitDesign", description: "Circuit Design", type: numType,},
+        {key: "Deaths", description: "Number of Fatalities", type: numType,},
+        {key: "Qualified", description: "Qualified Position", type: numType,}
+    ];
+
     xScale2 = d3.scalePoint()
-        .domain(d3.range(column.length))
+        .domain(d3.range(column2.length))
         .range([0, width]);
 
     yAxis2 = d3.axisLeft();
@@ -172,43 +169,47 @@ function setupParallel2() {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     canvas2 = d3.select("#canvas_vis2")
-        .attr("width", width * pixelRatio2)
-        .attr("height", height * pixelRatio2)
-        .style("width", width + "px")
-        .style("height", height + "px")
+        .attr("width", width * pixelRatio2 + "px")
+        .attr("height", height * pixelRatio2 + "px")
         .style("margin-top", margin.top + "px")
         .style("margin-left", margin.left + "px");
 
     context2 = canvas2.node().getContext("2d");
-    context2.globalCompositeOperation = 'darken';
-    context2.globalAlpha = 0.5;
-    context2.lineWidth = 2;
-    context2.scale(pixelRatio2, pixelRatio2);
 
     axes2 = svg2.selectAll(".axis")
-        .data(column)
+        .data(column2)
         .enter().append("g")
-        .attr("class", function(d) { return "axis " + d.key.replace(/ /g, "_"); })
-        .attr("transform", function(d,i) { return "translate(" + xScale2(i) + ")"; });
+        .attr("class", function(d) {
+            return "axis " + d.key;
+        })
+        .attr("transform", function(d,i) {
+            return "translate(" + xScale2(i) + ")";
+        });
 
     d3.csv("lemans4.csv").then(function(data) {
 
-        let color2 = d3.scaleOrdinal()
-            .domain(["CHENARD & WALCKER", "BENTLEY", "LORRAINE DIETRICH", "ALFA-ROMEO", "LAGONDA", "BUGATTI", "DELAHAYE", "FERRARI", "TALBOT LAGO", "JAGUAR", "MERCEDES- BENZ", "ASTON MARTIN", "FORD", "PORSCHE", "MATRA- SIMCA", "GULF", "RENAULT ALPINE", "SAUBER", "MAZDA", "PEUGEOT", "McLAREN", "BMW", "AUDI" ])
-        .range(d3["schemeDark2"]);
-
         data.forEach(function(d) {
-            column.forEach(function(e) {
+            column2.forEach(function(e) {
                 d[e.key] = !d[e.key] ? null : e.type.coerce(d[e.key]);
+                e.domain = e.type.extent(data.map(function(d) {
+                    return d[e.key];
+                }));
+                e.scale = e.type.yScale.copy().domain(e.domain);
             });
         });
 
-        column.forEach(function(dim) {
-            dim.domain = dim.type.extent(data.map(function(d) { return d[dim.key]; }));
-            dim.scale = dim.type.yScale.copy().domain(dim.domain);
+        let render = renderQueue(function (d) {
+            context2.strokeStyle = color(d.Manufacturer);
+            context2.beginPath();
+            let coords = project(d);
+            coords.forEach(function(d,i) {
+                if (d === null) {
+                    return;
+                }
+                context2.lineTo(d[0],d[1]);
+            });
+            context2.stroke();
         });
-
-        let render = renderQueue(draw).rate(1000);
 
         render(data);
 
@@ -223,24 +224,10 @@ function setupParallel2() {
                 return d.description;
             });
 
-        d3.selectAll(".axis.Manufacturer .tick text")
-            .style("fill", color2);
-
-        function draw(d) {
-            context2.strokeStyle = color2(d.Manufacturer);
-            context2.beginPath();
-            let coords = project(d);
-            coords.forEach(function(d,i) {
-                if (d === null) {
-                    return;
-                }
-                context2.lineTo(d[0],d[1]);
-            });
-            context2.stroke();
-        }
+        d3.selectAll(".axis.Manufacturer .tick text").style("fill", color);
 
         function project(d) {
-            return column.map(function(p, i) {
+            return column2.map(function(p, i) {
                 return d[p.key] === null ? null : [xScale2(i),p.scale(d[p.key])];
             });
         }
@@ -251,8 +238,22 @@ function setupParallel3() {
     let xScale3, yAxis3, svg3, canvas3, context3, axes3;
     let pixelRatio3 = window.devicePixelRatio;
 
+    var column3 = [
+        {key: "Manufacturer", description: "Winning Manufacturer", type: strType,},
+        {key: "Year", description: "Year of Race", type: dateType,},
+        {key: "EngineSizeLitre", description: "Engine Size (l)", type: numType,},
+        {key: "NoCylinders", description: "Number of Engine Cylinders", type: numType,},
+        {key: "Distance", description: "Distance Traveled (km)", type: numType,},
+        {key: "AveSpeed", description: "Average Lap Speed (km/h)", type: numType,},
+        {key: "Laps", description: "Number of Laps Completed", type: numType,},
+        {key: "FastestSpeed", description: "Fastest Recorded Speed (kph)", type: numType,},
+        {key: "CircuitDesign", description: "Circuit Design", type: numType,},
+        {key: "Deaths", description: "Number of Fatalities", type: numType,},
+        {key: "Qualified", description: "Qualified Position", type: numType,}
+    ];
+
     xScale3 = d3.scalePoint()
-        .domain(d3.range(column.length))
+        .domain(d3.range(column3.length))
         .range([0, width]);
 
     yAxis3 = d3.axisLeft();
@@ -264,43 +265,46 @@ function setupParallel3() {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     canvas3 = d3.select("#canvas_vis3")
-        .attr("width", width * pixelRatio3)
-        .attr("height", height * pixelRatio3)
-        .style("width", width + "px")
-        .style("height", height + "px")
+        .attr("width", width * pixelRatio3 + "px")
+        .attr("height", height * pixelRatio3 + "px")
         .style("margin-top", margin.top + "px")
         .style("margin-left", margin.left + "px");
 
     context3 = canvas3.node().getContext("2d");
-    context3.globalCompositeOperation = 'darken';
-    context3.globalAlpha = 0.5;
-    context3.lineWidth = 2;
-    context3.scale(pixelRatio3, pixelRatio3);
 
     axes3 = svg3.selectAll(".axis")
-        .data(column)
+        .data(column3)
         .enter().append("g")
-        .attr("class", function(d) { return "axis " + d.key.replace(/ /g, "_"); })
-        .attr("transform", function(d,i) { return "translate(" + xScale3(i) + ")"; });
+        .attr("class", function(d) {
+            return "axis " + d.key;
+        })
+        .attr("transform", function(d,i) {
+            return "translate(" + xScale3(i) + ")";
+        });
 
     d3.csv("lemans4.csv").then(function(data) {
 
-        let color3 = d3.scaleOrdinal()
-            .domain(["CHENARD & WALCKER", "BENTLEY", "LORRAINE DIETRICH", "ALFA-ROMEO", "LAGONDA", "BUGATTI", "DELAHAYE", "FERRARI", "TALBOT LAGO", "JAGUAR", "MERCEDES- BENZ", "ASTON MARTIN", "FORD", "PORSCHE", "MATRA- SIMCA", "GULF", "RENAULT ALPINE", "SAUBER", "MAZDA", "PEUGEOT", "McLAREN", "BMW", "AUDI" ])
-            .range(d3["schemeDark2"]);
-
         data.forEach(function(d) {
-            column.forEach(function(e) {
+            column3.forEach(function(e) {
                 d[e.key] = !d[e.key] ? null : e.type.coerce(d[e.key]);
+                e.domain = e.type.extent(data.map(function(d) {
+                    return d[e.key];
+                }));
+                e.scale = e.type.yScale.copy().domain(e.domain);
             });
         });
 
-        column.forEach(function(dim) {
-            dim.domain = dim.type.extent(data.map(function(d) { return d[dim.key]; }));
-            dim.scale = dim.type.yScale.copy().domain(dim.domain);
+        let render3 = renderQueue(function (d) {
+            context3.strokeStyle = color(d.Manufacturer);
+            context3.beginPath();
+            let coords = project(d);
+            coords.forEach(function(d) {
+                if (d !== null) {
+                    context3.lineTo(d[0],d[1]);
+                }
+            });
+            context3.stroke();
         });
-
-        let render3 = renderQueue(draw).rate(1000);
 
         render3(data);
 
@@ -320,7 +324,9 @@ function setupParallel3() {
             .each(function(d) {
                 d3.select(this).call(d.brush = d3.brushY()
                     .extent([[-10,0], [10,height]])
-                    .on("start", brushstart)
+                    .on("start", function () {
+                        d3.event.sourceEvent.stopPropagation();
+                    })
                     .on("brush", brush)
                     .on("end", brush)
                 )
@@ -329,34 +335,15 @@ function setupParallel3() {
             .attr("x", -8)
             .attr("width", 16);
 
-        function brushstart() {
-            d3.event.sourceEvent.stopPropagation();
-        }
-
-        d3.selectAll(".axis.Manufacturer .tick text").style("fill", color3);
-
-        function draw(d) {
-            context3.strokeStyle = color3(d.Manufacturer);
-            context3.beginPath();
-            let coords = project(d);
-            coords.forEach(function(d,i) {
-                if (d === null) {
-                    return;
-                }
-                context3.lineTo(d[0],d[1]);
-            });
-            context3.stroke();
-        }
+        d3.selectAll(".axis.Manufacturer .tick text").style("fill", color);
 
         function project(d) {
-            return column.map(function(p, i) {
+            return column3.map(function(p, i) {
                 return d[p.key] === null ? null : [xScale3(i),p.scale(d[p.key])];
             });
         }
 
         function brush() {
-            render3.invalidate();
-
             let actives = [];
             svg3.selectAll(".axis .brush")
                 .filter(function(d) {
@@ -379,8 +366,49 @@ function setupParallel3() {
             });
 
             context3.clearRect(0,0,width,height);
-            context3.globalAlpha = d3.min([0.85/Math.pow(selected.length,0.3),1]);
             render3(selected);
         }
     })
 }
+
+let renderQueue = (function(func) {
+    let _queue = [],                  // data to be rendered
+        _rate = 30,                 // number of calls per frame
+        _invalidate = function() {},  // invalidate last render queue
+        _clear = function() {};       // clearing function
+
+    let rq = function(data) {
+        if (data) rq.data(data);
+        _invalidate();
+        _clear();
+        rq.render();
+    };
+
+    rq.render = function() {
+        var valid = true;
+        _invalidate = rq.invalidate = function() {
+            valid = false;
+        };
+
+        function doFrame() {
+            if (!valid) return true;
+            var chunk = _queue.splice(0,_rate);
+            chunk.map(func);
+            timer_frame(doFrame);
+        }
+
+        doFrame();
+    };
+
+    rq.data = function(data) {
+        _invalidate();
+        _queue = data.slice(0);   // creates a copy of the data
+        return rq;
+    };
+
+    let timer_frame = window.requestAnimationFrame
+        || window.webkitRequestAnimationFrame
+        || function(callback) { setTimeout(callback, 17); };
+
+    return rq;
+});
